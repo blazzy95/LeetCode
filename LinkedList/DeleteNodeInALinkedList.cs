39e579456688ff5045bc4f8e9761b6b7c9d4cf25
Jitendra Selvam <jitendra3195@gmail.com>
2018-05-11 19:31:31 -0400

    Commit 237

---
+++ LinkedList/DeleteNodeInALinkedList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// 237. Delete Node in a Linked List
+/// </summary>
+
+namespace DeleteNodeInALinkedList
+{
+    class DeleteNodeInALinkedList
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+        public class Solution
+        {
+            public void DeleteNode(ListNode node)
+            {
+                node.val = node.next.val;
+                node.next = node.next.next;
+            }
+        }
+    }
+}
