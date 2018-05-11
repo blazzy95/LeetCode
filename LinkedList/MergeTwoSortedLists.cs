5ab4eee61e85a79009a54cfe3f635158f8a17030
Jitendra Selvam <jitendra3195@gmail.com>
2018-05-11 01:23:34 -0400

    LinkedLists

---
+++ LinkedList/MergeTwoSortedLists.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MergeTwoSortedLists
+{
+    class MergeTwoSortedLists
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            ListNode list = new ListNode(0);
+            ListNode node = list;
+            while (l1 != null || l2 != null)
+            {
+                if (l1 != null && l2 != null)
+                {
+                    if (l1.val < l2.val)
+                    {
+                        node.next = l1;
+                        l1 = l1.next;
+                    }
+                    else
+                    {
+                        node.next = l2;
+                        l2 = l2.next;
+                    }
+                    node = node.next;
+                }
+                else if (l1 == null)
+                {
+                    node.next = l2;
+                    break;
+                }
+                else if (l2 == null)
+                {
+                    node.next = l1;
+                    break;
+                }
+
+            }
+            return list.next;
+        }
+    }
+}
