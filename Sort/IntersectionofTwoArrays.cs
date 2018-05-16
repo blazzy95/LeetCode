7f7780136af5768d53ce0a7b83e32da8afc5d121
Jitendra Selvam <jitendra3195@gmail.com>
2018-05-16 04:28:45 -0400

    349. Intersection of Two Arrays

---
+++ Sort/IntersectionofTwoArrays.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// 349. Intersection of Two Arrays
+/// </summary>
+
+namespace Sort
+{
+    class IntersectionofTwoArrays
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            HashSet<int> set1 = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+
+            foreach(int i in nums1)
+            {
+                set1.Add(i);
+            }
+
+            foreach(int i in nums2)
+            {
+                if (set1.Contains(i))
+                    set2.Add(i);
+            }
+
+            return set2.ToArray<int>();
+
+        }
+    }
+}
