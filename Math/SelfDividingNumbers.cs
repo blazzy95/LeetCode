1f5e5da6a52211130775b32e95ea83bcf37a15c1
Jitendra Selvam <jitendra3195@gmail.com>
2018-06-10 19:27:22 -0400

    728. Self Dividing Numbers

---
+++ Math/SelfDividingNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// 728. Self Dividing Numbers
+/// </summary>
+
+namespace ConsoleApp4
+{
+    class SelfDividingNumbers
+    {
+        private static IList<int> SelfDividingNumber(int left, int right)
+        {
+            bool flag = true;
+            List<int> list = new List<int>();
+            for (; left <= right; left++)
+            {
+                string l = left.ToString();
+                if (l.Contains("0"))
+                    continue;
+                for(int i =0;i<l.Length;i++)
+                {
+                    if (left%Int32.Parse(l[i].ToString())!=0)
+                    {
+                        flag = false;
+                    }
+                }
+                if (flag)
+                {
+                    list.Add(left);
+                }
+                flag = true;
+
+            }
+            return list;
+        }
+    }
+}
