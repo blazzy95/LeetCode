d77504cd0a180e7fe30e6006979f9b3d4bbafe66
Jitendra Selvam <jitendra3195@gmail.com>
2018-06-10 08:05:52 -0400

    804. Unique Morse Code Words

---
+++ Strings/UniqueMorseCodeWords.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// Leet code 804. Unique Morse Code Words
+/// </summary>
+
+
+namespace ConsoleApp4
+{
+    class UniqueMorseCodeWords
+    {
+        public static int UniqueMorseRepresentations(string[] words)
+        {
+            string[] morseCode = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+
+            HashSet<string> set = new HashSet<string>();
+            
+            foreach (string word in words)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    sb.Append(morseCode[word[i] - 'a']);
+                }
+
+                if (!set.Contains(sb.ToString()))
+                    set.Add(sb.ToString());
+
+            }
+
+            return set.Count;
+        }
+    }
+}
