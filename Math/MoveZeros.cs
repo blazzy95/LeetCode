405374603a725814c35b50fc9e19623b26a8831b
Jitendra Selvam <jitendra3195@gmail.com>
2018-06-10 20:20:01 -0400

    283. Move Zeroes

---
+++ Math/MoveZeros.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/// <summary>
+/// 283. Move Zeroes
+/// </summary>
+
+namespace ConsoleApp4
+{
+    class MoveZeros
+    {
+        private static void MoveZeroes(int[] arr)
+        {
+            int i = 0,j=1;
+            while(j<arr.Length)
+            {
+                if (arr[i] == 0 && arr[j] == 0)
+                    j++;
+                else if (arr[i] == 0)
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    i++;
+                    j++;
+                }
+            }
+        }
+    }
+}
