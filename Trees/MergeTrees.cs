b48dd63e137f3a443af9b04d24d5e7ee438a3088
Jitendra Selvam <jselvam@iu.edu>
2018-06-26 16:21:05 -0400

    Added tree codes

---
+++ Trees/MergeTrees.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// 617. Merge Two Binary Trees
+/// </summary>
+
+namespace BinaryTree
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+    public class Solution
+    {
+        public static TreeNode Mergetrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 != null && t2 != null)
+            {
+                t1.left = MergeTrees(t1.left, t2.left);
+                t1.right = MergeTrees(t1.right, t2.right);
+                t1.val += t2.val;
+                return t1;
+            }
+            return t1 == null ? t2 : t1;
+        }
+    }
+}
---
+++ Trees/TwoSumIVInputIsABST.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// 653. Two Sum IV - Input is a BST
+/// </summary>
+
+namespace BinaryTree
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+    public class Solution
+    {
+        public bool FindTarget(TreeNode root, int k)
+        {
+            HashSet<int> set = new HashSet<int>();
+            Stack<TreeNode> nodes = new Stack<TreeNode>();
+
+            while (nodes.Count != 0 || root != null)
+            {
+                if (root != null)
+                {
+                    if (set.Contains(root.val))
+                        return true;
+                    set.Add(k - root.val);
+                    nodes.Push(root);
+                    root = root.left;
+                }
+                else
+                {
+                    root = nodes.Pop().right;
+                }
+            }
+
+            return false;
+        }
+    }
+}
