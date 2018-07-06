13601248486c0f5653ead64009240b98fcf249ce
Jitendra Selvam <jselvam@iu.edu>
2018-07-06 12:11:30 -0400

    added strings

---
+++ Strings/DetectCapital.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 520. Detect Capital
+/// </summary>
+
+namespace leetcodeLocal
+{
+    public class DetectCapital
+    {
+        public static bool DetectCapitalUse(string word)
+        {
+            bool res1 = true, res2 = true, res3 = true;
+
+            //Check if all are capital
+            foreach (var l in word)
+            {
+                if (l < 60 || l > 90)
+                {
+                    res1 = false;
+                    break;
+                }
+
+            }
+
+            //Check if starting is capital and remaining letters are lower case
+            if (word[0] >= 60 && word[0] <= 90)
+            {
+                for (int i = 1; i < word.Length; i++)
+                {
+                    if (word[i] >= 60 && word[i] <= 90)
+                    {
+                        res2 = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                res2 = false;
+            }
+
+            //check if all are lower case
+            foreach (var l in word)
+            {
+                if ((l >= 60 && l <= 90))
+                {
+                    res3 = false;
+                    break;
+                }
+            }
+
+            return res1||res2||res3;
+        }
+    }
+}
---
+++ Strings/ReverseWords.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 557. Reverse Words in a String III
+/// </summary>
+
+namespace leetcodeLocal
+{
+    class ReverseWords
+    {
+        public static string ReverseWords(string s)
+        {
+            string[] words = s.Split(' ');
+            StringBuilder sb = new StringBuilder();
+            foreach (var word in words)
+            {
+                sb.Append(Reverse(word));
+                sb.Append(" ");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string Reverse(string s)
+        {
+            if (s == null) return null;
+            char[] charArray = s.ToCharArray();
+            int len = s.Length - 1;
+
+            for (int i = 0; i < len; i++, len--)
+            {
+                charArray[i] ^= charArray[len];
+                charArray[len] ^= charArray[i];
+                charArray[i] ^= charArray[len];
+            }
+
+            return new string(charArray).TrimEnd();
+        }
+    }
+}
